2431eb87eb1ce19e68275cde60cdc6e5835eab0c
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-03-20 17:53:26 -0300

    Mejora

--- static/js/apps/Components/productA.js
+++ static/js/apps/Components/productA.js
@@ -5,15 +5,13 @@ import ContainerCart from './ContainerCart';
 
 //HACER UNA CONSTANTE QUE CONTENGA EL URL DE LA API
 const Product = () =>{
-
-
   
 return(
   catalogo.catalog.map((producto)=>
   <div className='productt' key={producto.id}>
     <img src={producto.imageURL}/>
     <h3>{producto.name}</h3>
-    <h3>{producto.price}</h3>
+    <h3>{`$${producto.price}`}</h3>
     <button className='buttonItems' onClick={this.Search =()=>{
       const actual = document.querySelector('.TotalR').textContent
       document.querySelector('.TotalR').innerHTML='';
